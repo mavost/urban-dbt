57c2c3fa74221ae6de54952f6d2e3eff4fa2f3ee
Markus von Steht <markus.von.steht@gmx.de>
2024-07-08 16:26:24 +0200

    changed macro to non-tree-besed naming convention

---
+++ dbt_norm_table/macros/generate_schema_name.sql
@@ -0,0 +1,31 @@
+{# standard - when working with on different dbs M2-DWH / M2-DWH_dev #}
+{% macro generate_schema_name(custom_schema_name, node) -%}
+
+    {%- set default_schema = target.schema -%}
+    {%- if custom_schema_name is none -%}
+
+        {{ default_schema }}
+
+    {%- else -%}
+
+        {{ custom_schema_name | trim }}
+
+    {%- endif -%}
+
+{%- endmacro %}
+
+{# standard, see, https://docs.getdbt.com/docs/build/custom-schemas #}
+{# {% macro generate_schema_name(custom_schema_name, node) -%}
+
+    {%- set default_schema = target.schema -%}
+    {%- if custom_schema_name is none -%}
+
+        {{ default_schema }}
+
+    {%- else -%}
+
+        {{ default_schema }}_{{ custom_schema_name | trim }}
+
+    {%- endif -%}
+
+{%- endmacro %} #}
