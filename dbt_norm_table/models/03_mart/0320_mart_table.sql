75ad91ba6d96e85e113849278c58edf51ed05775
Markus von Steht <markus.von.steht@gmx.de>
2024-07-16 21:44:12 +0200

    added incremental loading

--- dbt_norm_table/models/03_mart/0320_mart_table.sql
+++ dbt_norm_table/models/03_mart/0320_mart_table.sql
@@ -1,3 +1,21 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key=['"SpineID"', '"OrdRetInvoiceNo"', '"OrdRetInvoiceDate"', '"StockDimCode"', '"OrdRetCancellation"']
+    )
+}}
+
+-- rerun/materialize only 1 months of pre-existing data to cover the jump of 50 days of new data
+
+WITH
+get_max AS 
+(
+
+    SELECT DISTINCT
+        max(s."SpineValidTo") AS "MaxDate"
+    FROM ( SELECT DISTINCT "SpineValidTo" FROM {{ this }} ) s
+
+)
 
 SELECT 
     m."SpineID",
@@ -22,12 +40,24 @@ SELECT
     j2."StockDimCode",
     j2."StockDimDescription"
 FROM {{ ref('0301_orders_returns_facts') }} m
+
 INNER JOIN {{ ref('0311_customers_dim') }} j1
 ON j1."SpineID" = m."SpineID"
     AND j1."CustDimCustomerID" = m."OrdRetCustomersID"
     -- date ranges are not inclusive in ValidTo apart from the reporting spine's range
     AND j1."CustDimValidFrom" <= m."OrdRetInvoiceDate"::DATE
     AND m."OrdRetInvoiceDate"::DATE < j1."CustDimValidTo"
+
 INNER JOIN {{ ref('0312_stock_dim') }} j2
 ON j2."SpineID" = m."SpineID"
     AND j2."StockDimStockID" = m."OrdRetStockID"
+{% if is_incremental() %}
+
+WHERE m."SpineValidFrom"
+    >= (
+        SELECT
+            "MaxDate"
+            - INTERVAL '{{ var("overlap_interval") }}'
+        FROM get_max
+    )
+{% endif %}
